650b0dd333140b7b40893b02e6371077111da526
t-ilieva <ilieva0249@gmail.com>
2024-12-29 00:37:07 +0200

    Initial commit

---
+++ src/main/java/spring/ms/cars/CarManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package spring.ms.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementBackendApplication.class, args);
+	}
+
+}
